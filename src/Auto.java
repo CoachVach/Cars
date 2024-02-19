d9b5f5db57f8f2df0a03cee8f58e3e60bf4874fa
Erik Kovach <kovacherik@gmail.com>
2024-02-18 22:10:34 -0300

    Primer commit

---
+++ src/Auto.java
@@ -0,0 +1,30 @@
+public class Auto {
+    private int precio;
+    private int kilometros;
+    private int gasolina;
+    private int capacidad_gasolina;
+
+    public Auto(int precio,int kilometros,int capacidad){
+        this.precio = precio;
+        this.kilometros = kilometros;
+        this.capacidad_gasolina = capacidad;
+        this.gasolina = capacidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public int getKilometros() {
+        return kilometros;
+    }
+
+    public int getGasolina() {
+        return gasolina;
+    }
+
+    public int getCapacidad() {
+        return capacidad_gasolina;
+    }
+
+}
